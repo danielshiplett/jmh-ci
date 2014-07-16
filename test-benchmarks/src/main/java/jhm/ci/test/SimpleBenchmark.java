160759ce8f4477b51107d39d4f6cd0fb7aa45f76
Daniel Shiplett <daniel.shiplett@g2-inc.com>
2014-07-16 14:12:46 -0400

    Squashed commit of the following:
    commit 8074f74d1b17de890a0e77dc0c9d38771815f33b
    Author: Daniel Shiplett <daniel.shiplett@g2-inc.com>
    Date:   Wed Jul 16 14:06:00 2014 -0400
    
        Comment on the test benchmark class.
    
    commit ced729281d11ca25697bd20fa427662f2b24511c
    Author: Daniel Shiplett <daniel.shiplett@g2-inc.com>
    Date:   Wed Jul 16 14:03:58 2014 -0400
    
        Renamed the packages in the test benchmark project and removed unused test data files.
    
    commit a2d9bff328fff85f5c40f054ca5cf2c4dc42223a
    Author: Daniel Shiplett <daniel.shiplett@g2-inc.com>
    Date:   Wed Jul 16 14:02:21 2014 -0400
    
        Add a test benchmark project so we can test the plugin.

---
+++ test-benchmarks/src/main/java/jhm/ci/test/SimpleBenchmark.java
@@ -0,0 +1,49 @@
+package jhm.ci.test;
+
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.openjdk.jmh.annotations.Mode.AverageTime;
+import static org.openjdk.jmh.annotations.Scope.Thread;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.GenerateMicroBenchmark;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.logic.BlackHole;
+
+/**
+ * A really simple benchmark that will create list of Integers and then use the
+ * BlackHole to consume the list entries. This should provide a relatively quick
+ * benchmark that we can run when testing the plugin.
+ * 
+ * @author ddshipl
+ */
+@State(Thread)
+@BenchmarkMode(AverageTime)
+@OutputTimeUnit(NANOSECONDS)
+@Warmup(iterations = 1, time = 2, timeUnit = SECONDS)
+@Measurement(iterations = 2, time = 2, timeUnit = SECONDS)
+@Fork(1)
+@Threads(1)
+public class SimpleBenchmark {
+
+	@GenerateMicroBenchmark
+	public void benchmark(BlackHole hole) {
+		List<Integer> ints = new ArrayList<Integer>();
+
+		for (int i = 0; i < 10; i++) {
+			ints.add(i);
+		}
+
+		for (Integer i : ints) {
+			hole.consume(i);
+		}
+	}
+}
